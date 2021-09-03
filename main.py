e5c38dc4d03ff24ba7d930e218d495f88ecca1b4
Morne <mr.mornesnyman@gmail.com>
2021-09-03 15:06:05 +0200

    updated main.py

--- main.py
+++ main.py
@@ -8,23 +8,35 @@ api_key = "7b54a242ece91b73eb0897a7bbafadfd60dba21f"
 url = "https://stackoverflow.com/"
 
 #Define short_url function
-def short_url(data_json ):
+def short_url(url_string):
 
+# Set API Key for Bitly
     headers={'Authorization': f"{api_key}" }
 
+# Set url input
     data = '{ "long_url": "'f"{url}"'" }'
 
+# Request short url response
     response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
 
-    data_json = (response.json())
-    
-    print(data_json['link'])
+# Format response as json
+    response_json = (response.json())
+
+# Filter json response     
+    response_json_filterd = print(response_json['link'])
+
+# Convert filtered json response to string
+    url_string = print(str(response_json_filterd))
 
 # Define main function
-def main():
+def new():
 
 # Call short_url function
-    return short_url(data_json=())
+    return short_url(url_string=())
+
+def main():
+    
+    return new()
 
 if __name__ == "__main__":
     main()
