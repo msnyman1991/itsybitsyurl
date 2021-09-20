df45690f8acd3fff26d00dffd5773903775a0394
Morne <mr.mornesnyman@gmail.com>
2021-09-20 17:03:56 +0200

    added exceptions

--- app.py
+++ app.py
@@ -1,40 +1,48 @@
 import requests
-import json
-import requests
     
-# Bitly API key
+# Bitly API key.
 api_key = "7b54a242ece91b73eb0897a7bbafadfd60dba21f"
 
-# Long URL
+# User input long URL.
 url = input("Please enter a long URL: ")
-url_prefix = "http"
 
-#request = requests.get(url)
+# Validate user input URL.
+def validate_url():
+    check_url = requests.get(url)
+    return check_url.status_code
 
+# Generate short URL with Bitly.
 def short_url(url):
-     # Set API Key for Bitly
-    headers={'Authorization': f"{api_key}" }
+    try:
+        # Set API Key for Bitly.
+        headers={'Authorization': f"{api_key}" }
 
-    # Set url input
-    data = '{ "long_url": "'f"{url}"'" }'
+        # Set URL input.
+        data = '{ "long_url": "'f"{url}"'" }'
 
-    # Request short url response
-    response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
+        # Request short URL response.
+        response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
 
-    # Format response as json
-    shorl_url = (response.json()['link'])
+        # Format response as json.
+        shorl_url = (response.json()['link'])
 
-    print(f"The short URL is:\n{shorl_url}") 
+        # Print short URL.
+        print(f"The short URL is:\n{shorl_url}") 
 
+    # If domain missing from URL, throw error.
+    except KeyError:
+        print("Invalid URL. Possibly no domain was provided. Please check the URL.")  
+
+# Main
 def main():
-    if url_prefix in url:
-        return short_url(url)
-    else:
-        return short_url(f"{url_prefix}" + "://" + url)
+    try:   
+        # If URL returns code 200 or 301, create short URL.
+        if validate_url() == 200:
+            return short_url(url)
+        # If URL returns MissingSchema, fix URL schema and return short URL.
+    except requests.exceptions.MissingSchema:
+        return short_url("http" + "://" + url)
+    except requests.exceptions.ConnectionError:
+        # If connection cannot be established to the URL, throw error.
+        print("Connection error. Please check the URL.")
 main()
-
-# To Do: 
-# 1. Check if URL format is correct (Contains http// or https:// as prefix)
-# 2. If URL format is incorrect, update url with http:// or https:// prefix
-# 3. If URL format is correct, check if URL is reachable with status code 200
-# 4. If URL format is correct and URL returns status code 200, generate short URL.
